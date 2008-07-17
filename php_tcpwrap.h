40d6ec581386852626682b929423a9c3d9e2925f
Marcin Gibula <mg@php.net>
2008-07-17 07:09:04 +0000

    New version for release.

--- php_tcpwrap.h
+++ php_tcpwrap.h
@@ -24,7 +24,7 @@
 extern zend_module_entry tcpwrap_module_entry;
 #define phpext_tcpwrap_ptr &tcpwrap_module_entry
 
-#define PHP_TCPWRAP_VERSION "1.1.2"
+#define PHP_TCPWRAP_VERSION "1.1.3"
 
 #ifdef PHP_WIN32
 #define PHP_TCPWRAP_API __declspec(dllexport)
